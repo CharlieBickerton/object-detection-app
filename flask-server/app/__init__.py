4d86a6f00953fd2eba48ff9f4fca5733ba309a50
Charlie Bickerton <charlie@mimin.al>
2019-06-14 15:13:39 +0100

    imported flask login, bcrypt and set up POST register route to put user in mongo collection

--- flask-server/app/__init__.py
+++ flask-server/app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from app.config import Config
 
 mongo = PyMongo()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 
 
 def create_app(config_class=Config):
@@ -11,6 +15,8 @@ def create_app(config_class=Config):
     app.config.from_object(Config) # import config variables and execute for app
 
     mongo.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
 
     from app.main.routes import main
     app.register_blueprint(main)
--- flask-server/app/main/routes.py
+++ flask-server/app/main/routes.py
@@ -1,18 +1,25 @@
-from flask import request, Blueprint
-from app import mongo
+from flask import request, Blueprint, abort, jsonify
+from flask_login import login_user, current_user, logout_user, login_required
+from app import mongo, bcrypt
 
 
 main = Blueprint('main', __name__)
 
 
-@main.route("/api/user/register/")
+@main.route("/api/user/register", methods=['GET', 'POST'])
 def register():
+    if not request.json:
+        abort(400)
+    if current_user.is_authenticated:
+        return "Already authed"
     user = mongo.db.user
-    username = 'Test'
-    email = 'test@test.com'
-    user.insert({'username': username, 'email': email })
-    return 'registerd'
-
+    hashed_password = bcrypt.generate_password_hash(request.json['password']).decode('utf-8')
+    user.insert({
+      'username': request.json['username'],
+      'email': request.json['email'],
+      'password': hashed_password,
+    })
+    return jsonify({'registered': 'success'})
 
 @main.route("/")
 def about():
