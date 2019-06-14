ecbb5c9fcfff903c3b3d1814a7ceaa649e54e542
Charlie Bickerton <charlie@mimin.al>
2019-06-14 13:45:26 +0100

    installed flask-mongo and created a docker-compose-mongo file

--- flask-server/app/main/routes.py
+++ flask-server/app/main/routes.py
@@ -1,4 +1,6 @@
 from flask import request, Blueprint
+from flask_pymongo import PyMongo
+
 
 main = Blueprint('main', __name__)
 
@@ -9,7 +11,7 @@ main = Blueprint('main', __name__)
 #     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
 #     return render_template('home.html', posts=posts)
 
-
 @main.route("/")
 def about():
     return 'hi'
+
