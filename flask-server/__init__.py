ca74474c5c993b762f9ec2b02565dfa864e9f864
Charlie Bickerton <charlie@mimin.al>
2019-06-14 13:31:37 +0100

    created venv, installed flask and created a __init__ file

---
+++ flask-server/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    # app.config.from_object(Config) # import config variables and execute for app
+
+    # db.init_app(app)
+    # db_file_path = os.path.join(app.root_path, 'site.db')
+
+    # if not os.path.isfile(db_file_path): # if there is no db create db
+    #     print('db not found - creating db')
+    #     db.create_all()
+    # else:
+    #     print('db already created')
+
+    return app
