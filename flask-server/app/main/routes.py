f6a716bf078b060a49df9823ab01f47be2c163af
Charlie Bickerton <charlie@mimin.al>
2019-06-17 11:46:18 +0100

    register and login routes are working

--- flask-server/app/main/routes.py
+++ flask-server/app/main/routes.py
@@ -1,11 +1,20 @@
 from flask import request, Blueprint, abort, jsonify
-from flask_login import login_user, current_user, logout_user, login_required
-from app import mongo, bcrypt, JSONEncoder
+from flask_jwt_extended import (create_access_token, create_refresh_token,
+                                jwt_required, jwt_refresh_token_required, get_jwt_identity)
+from app import mongo, bcrypt, JSONEncoder, jwt
 from app.schemas import validate_user
 from bson.objectid import ObjectId
 
 
 main = Blueprint('main', __name__)
+
+
+@jwt.unauthorized_loader
+def unauthorized_response(callback):
+    return jsonify({
+        'ok': False,
+        'message': 'Missing Authorization Header'
+    }), 401
 
 
 @main.route("/api/user/register", methods=['POST'])
@@ -18,30 +27,66 @@ def register():
         user = data['data']
         user['password'] = bcrypt.generate_password_hash(request.json['password']).decode('utf-8')
         print(user)
-        mongo.db.user.insert_one(user)
+        mongo.db.users.insert_one(user)
         return jsonify({'ok': True, 'message': 'User created successfully!'}), 200
     else:
         return jsonify({'ok': False, 'message': 'Bad request parameters: {}'.format(data['message'])}), 400
 
 
+@main.route('/api/user/login', methods=['POST'])
+def auth_user():
+    ''' login endpoint '''
+    data = validate_user(request.json)
+    if data['ok']:
+        data = data['data']
+        user = mongo.db.users.find_one({'username': data['username'], 'email': data['email']})
+        if user and bcrypt.check_password_hash(user['password'], data['password']):
+            print('user', user)
+            del user['password']
+            access_token = create_access_token(identity=data)
+            refresh_token = create_refresh_token(identity=data)
+            user['token'] = access_token
+            user['refresh'] = refresh_token
+            return jsonify({'ok': True, 'data': user}), 200
+        else:
+            return jsonify({'ok': False, 'message': 'invalid username or password'}), 401
+    else:
+        return jsonify({'ok': False, 'message': 'Bad request parameters: {}'.format(data['message'])}), 400
+
+
+@main.route('/api/user/refresh', methods=['POST'])
+@jwt_refresh_token_required
+def refresh():
+    ''' refresh token endpoint '''
+    current_user = get_jwt_identity()
+    ret = {
+            'token': create_access_token(identity=current_user)
+    }
+    return jsonify({'ok': True, 'data': ret}), 200
+
+
 @main.route("/api/user", methods=['GET', 'DELETE', 'PATCH'])
+@jwt_required
 def user():
     # get by anything
     if request.method == 'GET':
         query = request.args
-        if not query['_id']:
-            print('_id')
-            data = mongo.db.user.find_one(query)
+        print(query)
+        if not query.get('_id'):
+            data = mongo.db.users.find_one(query)
         else:
+            print('in here:', query['_id'])
             query = {"_id" : ObjectId(query['_id'])}
-            data = mongo.db.user.find_one(query)
+            print(query)
+            data = mongo.db.users.find_one(query)
+            print(data)
         return jsonify(data), 200
 
     data = request.json
     # Delete by _id
     if request.method == 'DELETE':
         if data.get('_id') is not None:
-            db_response = mongo.db.user.delete_one({"_id" : ObjectId(data['_id'])})
+            db_response = mongo.db.users.delete_one({"_id" : ObjectId(data['_id'])})
             if db_response.deleted_count == 1:
                 response = {'ok': True, 'message': 'record deleted'}
             else:
@@ -56,26 +101,8 @@ def user():
         if data.get('query') is not None and data['query'].get('_id') is not None:
             query = { "_id": ObjectId(data['query']['_id']) }
             print('in else:', data['payload'])
-            mongo.db.user.update_one(
+            mongo.db.users.update_one(
                 query, {'$set': data['payload']})
             return jsonify({'ok': True, 'message': 'record updated'}), 200
         else:
-            return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400  
-
-
-@main.route("/api/user/login", methods=['POST'])
-def login():
-    if current_user.is_authenticated:
-        return "You are already logged in"
-    user = mongo.db.user.find_one_or_404({"username": request.json['username']})
-    print(user['password'])
-    if user and bcrypt.check_password_hash(user['password'], request.json['password']):
-        login_user(user)
-        return jsonify({"login":"success"})
-    else:
-        return jsonify({"login":"unsuccessful"})
-
-@main.route("/")
-def about():
-    return 'hi'
-
+            return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
