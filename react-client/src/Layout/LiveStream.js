34fadbd9ffadf007f0e35b293fd812654b25fb70
Charlie Bickerton <charlie@mimin.al>
2019-06-19 09:10:23 +0100

    livestream fix

--- react-client/src/Layout/LiveStream.js
+++ react-client/src/Layout/LiveStream.js
@@ -28,47 +28,48 @@ class LiveStream extends React.Component {
   streamPredictions = async () => {
     const video = await document.getElementsByTagName("video")[0]
     // draw canvas and frame
-    const predictions = await this.model.detect(video);
+    if (video) {
+      const predictions = await this.model.detect(video);
+      const canvas = document.getElementById("canvas");
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      const ctx = canvas.getContext('2d');
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      const font = "16px sans-serif";
+      ctx.font = font;
+      ctx.textBaseline = "top";
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      // const frame = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-    const canvas = document.getElementById("canvas");
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    const font = "16px sans-serif";
-    ctx.font = font;
-    ctx.textBaseline = "top";
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    // const frame = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      // predict on frame
 
-    // predict on frame
+      // const firstPrediction = predictions[0]
 
-    // const firstPrediction = predictions[0]
+      // render inference
+      predictions.forEach(prediction => {
+        const y = prediction.bbox[1];
+        const x = prediction.bbox[0];
+        const width = prediction.bbox[2];
+        const height = prediction.bbox[3];
+        // Draw the bounding box.
+        ctx.strokeStyle = "#00FFFF";
+        ctx.lineWidth = 2;
+        ctx.strokeRect(x, y, width, height);
+        // Draw the label background.
+        ctx.fillStyle = "#00FFFF";
+        const textWidth = ctx.measureText(prediction.class).width;
+        const textHeight = parseInt(font, 10); // base 10
+        ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+        // Draw the text last to ensure it's on top.
+        ctx.fillStyle = "#000000";
+        ctx.fillText(prediction.class, x, y);
+      });
 
-    // render inference
-    predictions.forEach(prediction => {
-      const y = prediction.bbox[1];
-      const x = prediction.bbox[0];
-      const width = prediction.bbox[2];
-      const height = prediction.bbox[3];
-      // Draw the bounding box.
-      ctx.strokeStyle = "#00FFFF";
-      ctx.lineWidth = 2;
-      ctx.strokeRect(x, y, width, height);
-      // Draw the label background.
-      ctx.fillStyle = "#00FFFF";
-      const textWidth = ctx.measureText(prediction.class).width;
-      const textHeight = parseInt(font, 10); // base 10
-      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
-      // Draw the text last to ensure it's on top.
-      ctx.fillStyle = "#000000";
-      ctx.fillText(prediction.class, x, y);
-    });
-    
-    // repeat using rAF
-    requestAnimationFrame(() => {
-      this.streamPredictions();
-    })
+      // repeat using rAF
+      requestAnimationFrame(() => {
+        this.streamPredictions();
+      })
+    }
   }
 
   // switchCamera = () => {
